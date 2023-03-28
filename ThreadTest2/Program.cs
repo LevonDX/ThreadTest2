7e78decd229cf01e67dff09bee4c0c102f327fec
Levon Hovhannisyan <lehovhan@microsoft.com>
2023-03-28 20:26:12 +0400

    Add project files.

---
+++ ThreadTest2/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace ThreadTest2
+{
+    internal class Program
+    {
+       
+        static void F1()
+        {
+            long s = 0;
+            for (int i = 0; i < 1E10; i++)
+            {
+                Console.WriteLine("F1");
+                Thread.Sleep(500);
+            }
+        }
+
+        static void F2()
+        {
+            long s = 0;
+            for (int i = 0; i < 1E10; i++)
+            {
+                Console.WriteLine("F2");
+                Thread.Sleep(500);
+            }
+        }
+
+        static void Main()
+        {
+            Thread t1 = new Thread(F1);
+            Thread t2 = new Thread(F2);
+
+            Console.WriteLine("F1 started");
+            t1.Start();
+
+            Console.WriteLine("F2 started");
+            t2.Start();
+
+            Console.WriteLine("F2 finished");
+        }
+    }
+}
