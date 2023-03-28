e2c6a815c3afe72c451f98026ef0e94876b12e9d
Levon Hovhannisyan <lehovhan@microsoft.com>
2023-03-28 21:01:35 +0400

    ParSum (wrong code)

--- ThreadTest2/Program.cs
+++ ThreadTest2/Program.cs
@@ -1,40 +1,74 @@
-﻿namespace ThreadTest2
+﻿using System.Diagnostics;
+using System.Threading;
+using ThreadState = System.Threading.ThreadState;
+
+namespace ThreadTest2
 {
     internal class Program
     {
-       
-        static void F1()
+        const long max = (long)1E9;
+        long parSum = 0;
+
+        long SerialSum()
         {
             long s = 0;
-            for (int i = 0; i < 1E10; i++)
+            for (long i = 0; i < max; i++)
             {
-                Console.WriteLine("F1");
-                Thread.Sleep(500);
+                s += i;
             }
+
+            return s;
         }
 
-        static void F2()
+        void F(long start, long end)
         {
-            long s = 0;
-            for (int i = 0; i < 1E10; i++)
+            // sum of start to end
+            for (long i = start; i < end; i++)
             {
-                Console.WriteLine("F2");
-                Thread.Sleep(500);
+                parSum += i;
             }
         }
 
         static void Main()
         {
-            Thread t1 = new Thread(F1);
-            Thread t2 = new Thread(F2);
+            Program p = new Program();
 
-            Console.WriteLine("F1 started");
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            long result = p.SerialSum();
+            sw.Stop();
+
+            Console.WriteLine($"Serial sum = {sw.ElapsedMilliseconds}");
+
+            ///////////////////////////////
+            
+            sw.Reset();
+
+            sw.Start();
+
+            Thread t1 = new Thread(() => p.F(0, max / 5));
+            Thread t2 = new Thread(() => p.F(max / 5, max / 5 * 2));
+            Thread t3 = new Thread(() => p.F(max / 5 * 2, max / 5 * 3));
+            Thread t4 = new Thread(() => p.F(max / 5 * 3, max / 5 * 4));
+            Thread t5 = new Thread(() => p.F(max / 5 * 4, max));
+
             t1.Start();
 
-            Console.WriteLine("F2 started");
             t2.Start();
+            t3.Start();
+            t4.Start();
+            t5.Start();
 
-            Console.WriteLine("F2 finished");
+            while (t1.ThreadState == ThreadState.Running ||
+                t2.ThreadState == ThreadState.Running ||
+                    t3.ThreadState == ThreadState.Running ||
+                    t4.ThreadState == ThreadState.Running ||
+                    t5.ThreadState == ThreadState.Running) ;
+          
+
+            sw.Stop();
+
+            Console.WriteLine($"Parallel sum = {sw.ElapsedMilliseconds}, result = {p.parSum}");
         }
     }
 }
